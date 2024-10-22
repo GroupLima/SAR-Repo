5d29e682e86dff86a0ddc167f4354b3e3d87517e
Holly Sinclair <114172664+hothbert@users.noreply.github.com>
2024-10-22 11:44:21 +0100

    Update app.blade.php

--- search-app/resources/views/layouts/app.blade.php
+++ search-app/resources/views/layouts/app.blade.php
@@ -6,9 +6,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title')</title> <!-- Default title, can be overridden -->
+    <link rel="stylesheet" href="resources/css/app.css">
 </head>
 <body>
-    @yield('style', 'resources.css.app')
     @include('layouts.basic-navbar')   <!-- insert php content from other views -->
     @yield('content') <!-- Section for different page content -->
 </body>
