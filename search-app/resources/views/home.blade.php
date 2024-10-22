adfaaf17af2889c7179504e24c13354df866feb2
Holly Sinclair <114172664+hothbert@users.noreply.github.com>
2024-10-22 10:41:57 +0100

    added saas team's work to home page

--- search-app/resources/views/home.blade.php
+++ search-app/resources/views/home.blade.php
@@ -3,5 +3,26 @@
 @extends('layouts.app')
 @section('title', 'Home Page')
 @section('content')
-    <h2>This is the home page</h2>
+    <!doctype html>
+    <html lang="en">
+        <head>
+            <title>Lima</title>
+        </head>
+    
+        <body onload="onPageLoaded()">
+            <h2>This is the home page</h2>
+            <div id = title>
+                <h1>SRA 2.0</h1>
+                
+            <p>Home</p> 
+            <p>Seach</p>    
+            <p>About</p>    
+            <p>hello</p>    
+            <p>hello</p>    
+            <p>hello</p>  
+        
+            </div>
+            <img src="images/university.png" alt="University Logo" class="bottom-image">
+        </body>
+    </html>
 @endsection
