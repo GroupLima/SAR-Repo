f90983cc129557bd60b5504270741631cae9d93e
Holly Sinclair <holly.sinclair2004@gmail.com>
2024-10-24 12:50:39 +0100

    Update search.blade.php

--- search-app/resources/views/home-views/search.blade.php
+++ search-app/resources/views/home-views/search.blade.php
@@ -1,15 +1,19 @@
 <!-- resources/views/search.blade.php -->
 <!-- put search components here -->
-<h3>Search Method</h3>
-<input type="radio" id="keywords" name="search-method">
-<label for keywords>Keywords</label>
-<input type="radio" id="phrase" name="search-method">
-<label for phrase>Phrase</label>
-<input type="radio" id="regularex" name="search-method">
-<label for regularex>Regular Expression</label>
-<input type="radio" id="word-start" name="search-method">
-<label for word-start>Word Start</label>
-<input type="radio" id="word-middle" name="search-method">
-<label for word-middle>Word Middle</label>
-<input type="radio" id="word-end" name="search-method">
-<label for word-end>Word End</label>
+<div id="search-methods"> 
+    <h3>Search Method</h3>
+    <label for keywords>Keywords</label>
+    <input type="radio" id="keywords" name="search-method">
+    <label for phrase>Phrase</label>
+    <input type="radio" id="phrase" name="search-method">
+    <label for regularex>Regular Expression</label>
+    <input type="radio" id="regularex" name="search-method">
+    <label for word-start>Word Start</label>
+    <input type="radio" id="word-start" name="search-method">
+    <label for word-middle>Word Middle</label>
+    <input type="radio" id="word-middle" name="search-method">
+    <label for word-end>Word End</label>
+    <input type="radio" id="word-end" name="search-method">
+</div>    
+<h3>Languages</h3>
+
