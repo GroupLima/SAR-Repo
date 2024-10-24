742723c718404a20d967b01e81bc87a1a4aa9456
Holly Sinclair <holly.sinclair2004@gmail.com>
2024-10-24 12:38:19 +0100

    added form elements for adv search
    added the search method buttons. someone talented can style it

--- search-app/resources/views/home-views/search.blade.php
+++ search-app/resources/views/home-views/search.blade.php
@@ -1,6 +1,15 @@
 <!-- resources/views/search.blade.php -->
-
-<h2>This is the search view</h2>
-<div>
-    <!-- put search components here -->
-</div>
+<!-- put search components here -->
+<h3>Search Method</h3>
+<input type="radio" id="keywords" name="search-method">
+<label for keywords>Keywords</label>
+<input type="radio" id="phrase" name="search-method">
+<label for phrase>Phrase</label>
+<input type="radio" id="regularex" name="search-method">
+<label for regularex>Regular Expression</label>
+<input type="radio" id="word-start" name="search-method">
+<label for word-start>Word Start</label>
+<input type="radio" id="word-middle" name="search-method">
+<label for word-middle>Word Middle</label>
+<input type="radio" id="word-end" name="search-method">
+<label for word-end>Word End</label>
