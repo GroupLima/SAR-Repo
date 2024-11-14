81a00e22817cb5319e1a742589e1186935d87a69
Andy <u48am22@abdn.ac.uk>
2024-11-14 14:01:11 +0000

    query only doing one volume

--- search-app/resources/python/XQuerySearch.py
+++ search-app/resources/python/XQuerySearch.py
@@ -2,6 +2,7 @@ from pathlib import Path
 import os
 from BaseXClient import BaseXClient
 from pathlib import Path
+import time
 
 # Creating session to be able to query XML files - aka database
 
@@ -11,33 +12,44 @@ class XQuerySearch():
     self.session = BaseXClient.Session("localhost", 49888, "admin", "admin")
     self.working_directory = working_directory
     self.working_directory = Path(__file__).resolve().parent.parent.parent / working_directory
-    self.results = []
+    self.results = ()  
   
   # Conduct XQuery on every XML file
   def search(self, user_xquery):
-    try:
-      # print("Searching for: " + user_xquery)
-      # OS walk not working as expected, need to debug - should be printing names of xml files
-      # print(self.working_directory)
-      for (root, volume, xml_file) in os.walk(self.working_directory): # Walk through root, volume dirs, and xml files
-        for file in xml_file:
-          if file.endswith(".xml"): # Simple check to ensure file to be queried is an XML to prevent errors
-            # self.session.execute("open " + file) # Use BaseX to open the XML file
-            db_name = Path(file).stem # Get file name without .xml for database name
-            self.session.execute(f"create db {db_name} {Path(root) / file}") # temp db for each file
-            a = self.session.query(user_xquery).execute() # Process query, then execute it on current db
-            # print(a)
-            self.results.append(a) # Add result of query to list
-            self.session.execute(f"drop db {db_name}") # Drop the database
-            self.session.execute("close")
-    except Exception as e:
-      print(e) # Error handling in case of search failure
+      try:
+          db_name = "temp-xquery-db" 
+          self.session.execute(f"create db {db_name}")  # Create the database
+
+          for root, directories, files in os.walk(self.working_directory):  # Walk through root, directories, and files
+                          for directory in directories:
+                              print(f"Discovered directory: {directory}")
+                          for file in files:
+                              print(f"Discovered file: {file}")
+                              print(f"{Path(root) / file}")
+                              if file.endswith(".xml"):  # Simple check to ensure file to be queried is an XML to prevent errors
+                                  self.session.execute(f"add to {db_name} {Path(root) / file}")  # Add each XML file to the database
+
+          a = self.session.query(user_xquery).execute()  # Process query, then execute it on the database
+          self.results += (a,)  # Add result of query to tuple
+
+          self.session.execute(f"drop db {db_name}")  # Drop the database
+          self.session.execute("close")
+      except Exception as e:
+          print(e)  # Error handling in case of search failure  
+  
 if __name__ == "__main__":
   xquery = XQuerySearch(str(Path("storage/app/test-xml-files"))) # Allows cross-platform pathing, likely deployed on Linux but development is on Windows/Mac
-  xquery.search("for $id in //@xml:id return $id") 
-  for result in xquery.results: # Prints query result with formatting
-        print(result)  
-  
+  start_time = time.time()
+  xquery.search("for $id in //@xml:id return $id")
+  # result_count = 0
+  # for result in xquery.results: # Prints query result with formatting
+  #   count = len(result.split("\n"))
+  #   result_count += count
+  print(xquery.results[0])
+  elapsed_time = time.time() - start_time
+  print(f"Elapsed time: {elapsed_time}")
+
+  # print(f"Total results: {result_count}")
   r"""
   Talk zone here
   
