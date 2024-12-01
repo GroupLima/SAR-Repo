9fe8324e244f2e135524135f631ce9debb9d524c
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2024-12-01 19:17:08 +0000

    edited search controller

--- search-app/resources/python/XQuerySearch.py
+++ search-app/resources/python/XQuerySearch.py
@@ -1,9 +1,8 @@
+import time, uuid, os
 from pathlib import Path
-import os
 from BaseXClient import BaseXClient
 from pathlib import Path
-import time
-import uuid
+import xml.sax.saxutils as saxutils
 
 # Creating session to be able to query XML files - aka database
 
@@ -33,21 +32,18 @@ class XQuerySearch():
                     # Simple check to ensure file to be queried is an XML to prevent errors
                     if file.endswith(".xml"):
                         # Add each XML file to the database
-                        self.session.execute(f"add to {self.db_name} {
-                                             Path(root) / file}")
+                        self.session.execute(f"add to {self.db_name} {Path(root) / file}")
 
             # Process query, then execute it on the databa
             # Issue is it is returning one massive string
             # I need it in a better data structure format but the limitation of basexclient is that it only returns strings with execute() and there aren't any other ways to parse an XQuery
             # Needed in a dictionary ideally because when it is returned to the frontend it should have it's xml:id attached so that it can immediately link to that entry within the browse section
             actual_results = self.session.query(user_xquery).execute()
-            # print(actual_results)
             # Remove carriage return for correct writing to file
             formatted_results = actual_results.replace("\r", "")
             # For testing purposes, write to file as terminal has string output limit in VSCode
             with open(f"./search-app/storage/app/{self.db_name}_results.txt", "w") as result_file:
                 result_file.write(formatted_results)
-            # print(self.session.query(user_xquery).full())
 
             # Drop the database
             self.session.execute(f"drop db {self.db_name}")
@@ -59,6 +55,8 @@ class XQuerySearch():
 if __name__ == "__main__":
     # Allows cross-platform pathing, likely deployed on Linux but development is on Windows/Mac
     xquery = XQuerySearch(str(Path("storage/app/xml-files")))
+    # Sanitise query to prevent common attacks (e.g., overload server and injection attacks)
+    xquery = saxutils.escape(xquery)
     start_time = time.time()
     # Have to add declaration of namespace in case user doesn't, plus they shouldn't be expected to
     namespace = 'declare namespace ns = "http://www.tei-c.org/ns/1.0";'
@@ -75,6 +73,4 @@ if __name__ == "__main__":
         '<!--NEW_ENTRY_FLAG-->'
     )
     """
-    # xquery.search(namespace+user_xquery)
     xquery.search(final_xquery)
-    # with open(f"{xquery.db_name}_results.txt", "r") as result_file:
