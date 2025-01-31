bcd7d84963fba6964d672de7495c890cf82f096a
Holly Sinclair <holly.sinclair2004@gmail.com>
2025-01-31 16:24:17 +0000

    first draft of basic search model

---
+++ search-app/back-end/resources/python/data-models/model_basic.py
@@ -0,0 +1,12 @@
+"""
+A model that contains only the arguments required for Basic Search. This will be used to determine
+which search functions need to be executed.
+"""
+
+class BasicSearchModel(user_input, variants, number_results, order_by, search_method):
+    def __init__():
+        self.user_input = user_input # String
+        self.variants = variants # String
+        self.number_results = number_results # Integer
+        self.search_method = search_method # Integer
+        self.order_by = order_by # String
