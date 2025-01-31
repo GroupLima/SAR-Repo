31ca91f5baa7784aa5a816e0dd481ea6cd97bd56
Holly Sinclair <holly.sinclair2004@gmail.com>
2025-01-31 16:39:45 +0000

    first draft of advanced search model
    all edited data types on basic search model

---
+++ search-app/back-end/resources/python/data-models/model_advanced.py
@@ -0,0 +1,19 @@
+"""
+A model that contains only the arguments required for Advanced Search. This will be used to determine
+which search functions need to be executed.
+"""
+
+class AdvancedSearchModel(user_input, variants, number_results, order_by, search_method, language, page, volume paragraph, entry_id, from_date, to_date):
+    def __init__():
+        self.user_input = user_input # String
+        self.variants = variants # Integer
+        self.number_results = number_results # Integer
+        self.search_method = search_method # String
+        self.order_by = order_by # String
+        self.language = language # String or Null
+        self.page = page # Integer or Null
+        self.volume = volume # Integer or Null
+        self.paragraph = paragraph # String or Null
+        self.entry_id = entry_id # String or Null
+        self.from_date = from_date # String or Null
+        self.to_date = to_date # String or Null
--- search-app/back-end/resources/python/data-models/model_basic.py
+++ search-app/back-end/resources/python/data-models/model_basic.py
@@ -6,7 +6,7 @@ which search functions need to be executed.
 class BasicSearchModel(user_input, variants, number_results, order_by, search_method):
     def __init__():
         self.user_input = user_input # String
-        self.variants = variants # String
+        self.variants = variants # Integer
         self.number_results = number_results # Integer
-        self.search_method = search_method # Integer
+        self.search_method = search_method # String
         self.order_by = order_by # String
