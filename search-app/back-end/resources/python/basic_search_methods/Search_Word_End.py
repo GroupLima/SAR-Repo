dcdc90ea8bb21d3a4a786a7f842e98b72d9d86f4
Holly Sinclair <holly.sinclair2004@gmail.com>
2025-02-06 14:43:42 +0000

    maybe possibly working word-end
    lets hope there's no bugs

--- search-app/back-end/resources/python/basic_search_methods/Search_Word_End.py
+++ search-app/back-end/resources/python/basic_search_methods/Search_Word_End.py
@@ -20,6 +20,21 @@ class Search_Word_End(BasicSearch):
         """
         # write code here
 
+    def find_matches_in(self, content):
+        # Extract words and their start indices
+        words_to_compare = [(word.group(), word.start()) for word in re.finditer(r'\S+', content)]
 
+        #work in progress :')
+        results = []
+        for word, index in words_to_compare:
+            if not word or len(word) < self.qlen-2:
+                continue
+            # Check similarity score for words that may or may not match with the query
+            # gets the first x number of characters of the word
+            score = fuzz.ratio(self.query, word[len(word)-qlen:])
+
+            if score >= self.variance:
+                results.append((word, score, index))
+        return results
         
         pass
