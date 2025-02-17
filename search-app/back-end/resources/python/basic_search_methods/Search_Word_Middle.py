72db191821ebd7d0fb0a2b7f6c1402df4050ee94
Holly Sinclair <holly.sinclair2004@gmail.com>
2025-02-17 21:07:38 +0000

    started word middle
    successfully finds the query within words, but also finds the words within the query which we need to eliminate, i.e. returning "lly" when searching for holly as well as all words containing "holly"

--- search-app/back-end/resources/python/basic_search_methods/Search_Word_Middle.py
+++ search-app/back-end/resources/python/basic_search_methods/Search_Word_Middle.py
@@ -1,14 +1,14 @@
 from rapidfuzz import process, fuzz
 from basic_search_methods.Search_Method_Interface import Search_Method
+import re
 
 class Search_Word_Middle(Search_Method):
     
-    def __init__(self, **kwargs):
-        self.query = kwargs.get('query')
-        self.json_entries = kwargs.get('json_entries')
-        self.window_size = kwargs.get('window_size')
-        self.variance = kwargs.get('variance')
-        
+    def __init__(self, query, qlen, variance, json_entries):
+        self.query = query
+        self.qlen = qlen
+        self.variance = variance
+        self.json_entries = json_entries
         super().__init__()
 
 
@@ -17,8 +17,17 @@ class Search_Word_Middle(Search_Method):
         use rapid fuzz to extract all the matches in a single entry content
         specifically, use process.extract function and return the result
         """
-        # write code here
+        # Extract words and their start indices
+        words_to_compare = [(word.group(), word.start()) for word in re.finditer(r'\S+', content)]
 
-
-        
-        pass
+        #work in progress :')
+        results = []
+        for word, index in words_to_compare:
+            if not word or len(word) < self.qlen-2:
+                continue
+            # Check similarity score for words that may or may not match with the query
+            # rapidfuzz searches within the whole word
+            score = fuzz.partial_ratio(self.query, word)
+            if score >= self.variance:
+                results.append((word, score, index))
+        return results
--- search-app/back-end/resources/python/data_models/model_word_middle.py
+++ search-app/back-end/resources/python/data_models/model_word_middle.py
@@ -2,10 +2,12 @@ import re
 
 class Word_Middle():
     def __init__(self, user_input):
-        pass
+        self.query = self.normalize_query(user_input)
+        self.qlen = self.get_qlen(user_input)
 
-
-"""
-
-"""
+    def get_qlen(self, user_input):
+        return max(1, len(user_input))
+    
+    def normalize_query(self, user_input):
+        return re.sub(r'\s+', '', user_input.strip())
 
