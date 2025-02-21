3c4c39f208cf737a4553552feae1ef7c2c9209aa
caitlinthaeler <114182834+caitlinthaeler@users.noreply.github.com>
2025-02-21 16:06:10 +0000

    word start case sensitivity might work

--- search-app/back-end/resources/python/basic_search_methods/Search_Word_End.py
+++ search-app/back-end/resources/python/basic_search_methods/Search_Word_End.py
@@ -4,15 +4,21 @@ import re
 
 class Search_Word_End(Search_Method):
     
-    def __init__(self, query, qlen, variance, json_entries):
+    def __init__(self, query, qlen, variance, json_entries, case_sensitive=False):
         self.query = query
         self.qlen = qlen
         self.variance = variance # Integer
         self.json_entries = json_entries
-        
+        self.case_sensitive = case_sensitive
         super().__init__()
+        
 
     def find_matches_in(self, content):
+        """
+        use rapid fuzz to extract all the matches in a single entry content
+        specifically, use process.extract function and return the result
+        """
+        _content = content.lower() if self.case_sensitive else content
         # Extract words and their start indices
         words_to_compare = [(word.group(), word.start()) for word in re.finditer(r'\S+', content)]
 
--- search-app/back-end/resources/python/basic_search_methods/Search_Word_Start.py
+++ search-app/back-end/resources/python/basic_search_methods/Search_Word_Start.py
@@ -4,17 +4,17 @@ import re
 
 class Search_Word_Start(Search_Method):
 
-    def __init__(self, query, qlen, variance, json_entries):
+    def __init__(self, query, qlen, variance, json_entries, case_sensitive=False):
         self.query = query
         self.qlen = qlen
         self.variance = variance # Integer
         self.json_entries = json_entries
+        self.case_sensitive = case_sensitive
         super().__init__()
         
     def find_matches_in(self, content):
         # Extract words and their start indices
         words_to_compare = [(word.group(), word.start()) for word in re.finditer(r'\S+', content)]
-
         #work in progress :')
         results = []
         for word, index in words_to_compare:
@@ -22,7 +22,10 @@ class Search_Word_Start(Search_Method):
                 continue
             # Check similarity score for words that may or may not match with the query
             # gets the first x number of characters of the word
-            score = fuzz.ratio(self.query, word[:min(len(self.query), len(word))])
+            if not self.case_sensitive:
+                score = fuzz.ratio(self.query.lower(), word.lower()[:min(len(self.query), len(word))])
+            else:
+                score = fuzz.ratio(self.query, word[:min(len(self.query), len(word))])
 
             if score >= self.variance:
                 results.append((word, score, index))
