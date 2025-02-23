89b6eff3d847fb9ac65de2b9e46e02a6a900fb00
Haziel Osunde <114499382+Haz1el@users.noreply.github.com>
2025-02-23 16:16:10 +0000

    HomePageT
    HomePageT

---
+++ search-app/front-end/src/components/Volume.Selector.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h2>Volume Selection</h2>
+  
+      <!-- "Select All" and "Deselect All" Buttons -->
+      <button @click="selectAll" class="button">Select All Volumes</button>
+      <button @click="deselectAll" class="button">Deselect All</button>
+  
+      <!-- Volume checkboxes -->
+      <div class="volume-container">
+        <label v-for="(vol, index) in volumes" :key="index">
+          <input type="checkbox" v-model="selectedVolumes" :value="vol" class="volume-checkbox" />
+          {{ vol }}
+        </label>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        volumes: [1, 2, 3, 4, 5, 6, 7, 8], // Volume numbers
+        selectedVolumes: [] // Holds selected volume numbers
+      };
+    },
+    methods: {
+      selectAll() {
+        this.selectedVolumes = [...this.volumes]; // Select all volumes
+      },
+      deselectAll() {
+        this.selectedVolumes = []; // Deselect all volumes
+      }
+    }
+  };
+  </script>
+  
+  <style scoped>
+  @import '@/public/sass/app.css';
+  </style>
+  
