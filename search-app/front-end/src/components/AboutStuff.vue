f58f1c791d7bec55e171600bda294860a92bf71d
rleslie0522 <u05rl22@abdn.ac.uk>
2025-02-25 11:41:37 +0000

    about content/formatting, footer fixes, darkmode edits

--- search-app/front-end/src/components/AboutStuff.vue
+++ search-app/front-end/src/components/AboutStuff.vue
@@ -1,5 +1,6 @@
 <script setup>
 import router from '@/router';
+import Footer from '@/components/Footer.vue';
 </script>
 
 <template>
@@ -7,70 +8,26 @@ import router from '@/router';
     <div class="row">
       <div class="column">
         <h2>Aberdeen Burgh Registers</h2>
-        <div>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
+        <div class = "about-text">
+          <br>
+          <p>The Aberdeen Registers Online (ARO) corpus is a comprehensive digitised transcription of the first eight volumes of the Aberdeen Burgh Registers, the oldest surviving civic government records in Scotland. Spanning 113 years from 1398 (volume one) to 1511 (volume 8) these records detail a wide range of civic and legal activities including court proceedings, ordinances, elections, dispositions of property and correspondence. They provide precious insights into the lives, laws and customs of people living in 14th-century Aberdeen.  These books contain the earliest and most comprehensive archive of civic government records in Scotland and as such give historians the unique opportunity to trace and analyse patterns in the legal proceedings, governance, and social order of an urban city in the late Middle Ages.</p>
+          <br>
+          <img src="@/assets/images/aberdeenregisters1.jpg" alt="Registers Book Stack"/>
         </div>
       </div>
       <div class="column">
         <h2>Meet the Team</h2>
-        <div>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
+        <div class = "about-text">
+          <br>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <br>
         </div>
       </div>
     </div>
   </div>
+
+  <div>
+    <Footer />
+  </div>
+
 </template>
--- search-app/front-end/src/components/Navbar.vue
+++ search-app/front-end/src/components/Navbar.vue
@@ -12,12 +12,7 @@ const toggleDark = useToggle(isDark);
     <nav id="navbar" class="navbar">
         <div class="nav-content">
             <RouterLink to="/" class="logo-section">
-                <div class="text-container">
-                    <!-- New container for text -->
-                    <div class="logo-text">SAR</div>
-                    <div class="logo-subtext">Search Aberdeen Registers</div>
-                </div>
-                <img :src="townLogo" alt="Town Logo" class="logo-image">
+                <img src="@/assets/images/logo-current3.png" alt="SAR Logo"/>
             </RouterLink>
             <div class="nav-links">
                 <RouterLink to="/">
