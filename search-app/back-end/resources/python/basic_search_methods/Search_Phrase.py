3fb05fbe0d351ad9e66889b68240025f0065a113
Holly Sinclair <holly.sinclair2004@gmail.com>
2025-03-19 04:12:58 +0000

    fixed it
    still some kinks need ironing out

--- search-app/back-end/resources/python/basic_search_methods/Search_Phrase.py
+++ search-app/back-end/resources/python/basic_search_methods/Search_Phrase.py
@@ -22,21 +22,28 @@ class Search_Phrase(Search_Method):
         # time, i think. There will be less iterations
         for i in range(len(words_to_compare)):
             phrase_words = []
-            phrase_words.append(words_to_compare[i][0])
             start_pos = words_to_compare[i][1]
-            for j in range(i+1, len(words_to_compare)):
+            for j in range(i, len(words_to_compare)):
                 # different lengths of phrase
                 phrase_words.append(words_to_compare[j][0])
                 phrase = " ".join(phrase_words)
 
-                if len(phrase_words)>len(self.query.split())*2.5:
+                if (len(phrase) > self.qlen*3) or ():
+                    # skip phrases that are too long
                     break
+                if len(phrase) < self.qlen/3:
+                    # skip phrases that are too short
+                    continue
+
                 # Check similarity score for words that may or may not match with the query
-                score = get_fuzzy_score(self.query, phrase)
+                if not self.case_sensitive:
+                    score = get_fuzzy_score(self.query.lower(), phrase.lower())
+                else:
+                    score = get_fuzzy_score(self.query, phrase)
 
                 if score >= self.variance:
                     results.append((phrase, score, start_pos))
-                    break
+                    break # don't bother searching longer phrases
 
         return results
 
