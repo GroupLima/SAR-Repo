2f259fc094a711fbc7faef07aa0456d54a0709a4
Holly Sinclair <holly.sinclair2004@gmail.com>
2025-04-24 17:23:46 +0100

    allow advanced search without query input

--- search-app/back-end/resources/python/Search.py
+++ search-app/back-end/resources/python/Search.py
@@ -205,11 +205,22 @@ class Search():
         #self.order_by(self.sort_criteria)
 
     def start(self):
+        if self.query == "":
+            self.query = "*"
         qt = self.params['qt'] # query type: basic or advanced
-        search_obj = Basic_Search(self.search_method, self.query, self.variance, self.json_entries) # pass in parameters for basic search
-        self.matches = search_obj.find_matches()
+        
+        if self.query != "*":  
+            search_obj = Basic_Search(self.search_method, self.query, self.variance, self.json_entries) # pass in parameters for basic search
+            self.matches = search_obj.find_matches()
         
         if qt == 'advanced_search':
+            if self.query == "*":
+                for entry_id in self.json_entries.keys():
+                    self.matches[entry_id] = {
+                        'accuracy_score' : 100,
+                        'match_frequency' : 0,
+                        'matches' : []}
+
             advs_entries = {}
             for entry_id in self.matches.keys():
                 advs_entries[entry_id] = self.json_entries[entry_id]
