8f84261b7451da83215b06a2529121ecbb0b37ee
rleslie0522 <u05rl22@abdn.ac.uk>
2025-05-02 13:31:49 +0100

    update info to be more accurate

--- search-app/front-end/src/components/HomeStuff.vue
+++ search-app/front-end/src/components/HomeStuff.vue
@@ -10,7 +10,7 @@ import SearchFields from '@/components/SearchFields.vue';
             <div class="home-description">
                 <h1>Discover 7 Volumes</h1>
                 <br>
-                <p><b>Search Aberdeen Registers</b> is a digital tool that allows you to explore the rich historical records of Aberdeen’s burgh registers. These records date back to the 16th century and offer a unique insight into the city’s legal, social, and economic history. Whether you're a researcher, student, or history enthusiast, this tool makes it easy to search, discover, and learn from Aberdeen’s past.</p>
+                <p><b>Search Aberdeen Registers</b> is a digital tool that allows you to explore the rich historical records of Aberdeen’s burgh registers. These records date back to the 14th century and offer a unique insight into the city’s legal, social, and economic history. Whether you're a researcher, student, or history enthusiast, this tool makes it easy to search, discover, and learn from Aberdeen’s past.</p>
             </div>
         </header>
         <!-- Main Section -->
