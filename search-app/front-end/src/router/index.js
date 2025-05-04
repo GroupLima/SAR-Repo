87dce0c3b83b6ad2d43785121a9e96a81c7c52da
Haziel Osunde <114499382+Haz1el@users.noreply.github.com>
2025-05-04 03:52:06 +0100

    .

--- search-app/front-end/src/router/index.js
+++ search-app/front-end/src/router/index.js
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import BrowseView from '@/views/BrowseView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +11,8 @@ const router = createRouter({
     {
       path: '/browse',
       name: 'browse',
-      component: BrowseView.vue,
+      component: () => import('@/views/BrowseView.vue'),
+
     },
     {
       path: '/search',
