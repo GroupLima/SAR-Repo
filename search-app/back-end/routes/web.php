1a0af39da7d2dce9deb249e551bc708b897ec564
Haziel Osunde <114499382+Haz1el@users.noreply.github.com>
2025-05-04 01:55:46 +0100

    c

--- search-app/back-end/routes/web.php
+++ search-app/back-end/routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\RouteController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\TestController;
 use App\Http\Controllers\RecordController;
+use Illuminate\Support\Facades\File;
 
 
 /*
@@ -20,4 +21,9 @@ Route::prefix('/api')->group(function () {
     Route::get('/volumes', [RecordController::class, 'getVolumes']);
     Route::get('/records', [RecordController::class, 'getRecords']);
     Route::get('/records/{id}/xml', [RecordController::class, 'getXml']);
+    
 });
+
+Route::get('/{any}', function () {
+    return File::get(public_path('index.html'));
+})->where('any', '.*');
